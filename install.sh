0efa2349854f76aebfd7eab22bb533d0ec52e8af
Aron Bordin <aron.bordin@gmail.com>
2017-01-20 15:53:02 -0200

    fixes

--- install.sh
+++ install.sh
@@ -2,14 +2,21 @@
 
 echo "Installing symbolic links"
 
+rm -rf ~/.config/fish
+rm -rf ~/.config/i3
+rm -rf ~/.config/nvim
+rm -rf ~/.config/rofi
+rm -rf ~/.config/teiler
+rm -rf ~/.config/tmux
+rm -f ~/.tmux.conf
 
-ln -s ~/Programming/GitHub/dotconfig/fish/ ~/.config/fish
-ln -s ~/Programming/GitHub/dotconfig/i3/ ~/.config/i3
-ln -s ~/Programming/GitHub/dotconfig/nvim/ ~/.config/nvim
-ln -s ~/Programming/GitHub/dotconfig/rofi/ ~/.config/rofi
-ln -s~/Programming/GitHub/dotconfig/teiler/ ~/.config/teiler
-ln -s ~/Programming/GitHub/dotconfig/tmux/ ~/.config/tmux
-ln -s ~/Programming/GitHub/dotconfig/.tmux.conf ~/
+ln -s /home/aron/Programming/Github/dotconfig/fish/ /home/aron/.config/fish
+ln -s /home/aron/Programming/Github/dotconfig/i3/ /home/aron/.config/i3
+ln -s /home/aron/Programming/Github/dotconfig/nvim/ /home/aron/.config/nvim
+ln -s /home/aron/Programming/Github/dotconfig/rofi/ /home/aron/.config/rofi
+ln -s /home/aron/Programming/Github/dotconfig/teiler/ /home/aron/.config/teiler
+ln -s /home/aron/Programming/Github/dotconfig/tmux/ /home/aron/.config/tmux
+ln -s /home/aron/Programming/Github/dotconfig/.tmux.conf /home/aron/
 
 echo "Done with symlinks"
 
