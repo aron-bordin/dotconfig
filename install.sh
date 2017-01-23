151e7a58a54455a6c5960ed2a0411a2ef8d29dad
Aron Bordin <aron.bordin@gmail.com>
2017-01-23 02:26:28 -0200

    update

--- install.sh
+++ install.sh
@@ -1,22 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Installing symbolic links"
 
 rm -rf ~/.config/fish
 rm -rf ~/.config/i3
-rm -rf ~/.config/nvim
 rm -rf ~/.config/rofi
+rm -rf ~/.vimrc
+rm -rf ~/.config/termite
 rm -rf ~/.config/teiler
 rm -rf ~/.config/tmux
 rm -f ~/.tmux.conf
 
-ln -s /home/aron/Programming/Github/dotconfig/fish/ /home/aron/.config/fish
-ln -s /home/aron/Programming/Github/dotconfig/i3/ /home/aron/.config/i3
-ln -s /home/aron/Programming/Github/dotconfig/nvim/ /home/aron/.config/nvim
-ln -s /home/aron/Programming/Github/dotconfig/rofi/ /home/aron/.config/rofi
-ln -s /home/aron/Programming/Github/dotconfig/teiler/ /home/aron/.config/teiler
-ln -s /home/aron/Programming/Github/dotconfig/tmux/ /home/aron/.config/tmux
-ln -s /home/aron/Programming/Github/dotconfig/.tmux.conf /home/aron/
+ln -s /home/aron/Programming/GitHub/dotconfig/fish/ /home/aron/.config/fish
+ln -s /home/aron/Programming/GitHub/dotconfig/i3/ /home/aron/.config/i3
+ln -s /home/aron/Programming/GitHub/dotconfig/rofi/ /home/aron/.config/rofi
+ln -s /home/aron/Programming/GitHub/dotconfig/teiler/ /home/aron/.config/teiler
+ln -s /home/aron/Programming/GitHub/dotconfig/termite/ /home/aron/.config/termite
+ln -s /home/aron/Programming/GitHub/dotconfig/tmux/ /home/aron/.config/tmux
+ln -s /home/aron/Programming/GitHub/dotconfig/.tmux.conf /home/aron/
+ln -s /home/aron/Programming/GitHub/dotconfig/.vimrc /home/aron/
 
 echo "Done with symlinks"
 
@@ -25,5 +27,5 @@ echo "Installing deps"
 rm -rf /home/aron/.config/base16-shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
-rm -rf /home/aron/.config/nvim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git /home/aron/.config/nvim/bundle/Vundle.vim
+rm -rf /home/aron/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git /home/aron/.vim/bundle/Vundle.vim
