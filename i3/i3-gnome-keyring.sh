2716daa4db7dd2b94f90a1eecd2f765941a7d113
Aron Bordin <aron.bordin@gmail.com>
2017-02-12 02:17:52 -0200

    new coding session, new vim plugins

--- i3/i3-gnome-keyring.sh
+++ i3/i3-gnome-keyring.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-if [ "$DESKTOP_SESSION" = "i3" ]; then
-    eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
-    export SSH_AUTH_LOCK
-    echo "OK"
-    echo $SSH_AUTH_LOCK
-fi
+eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
+export SSH_AUTH_LOCK
+echo "OK"
+echo $SSH_AUTH_LOCK
