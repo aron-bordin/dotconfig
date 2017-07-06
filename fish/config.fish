22d475969530e676d26957054659edde2065fb14
Aron Bordin <aron.bordin@gmail.com>
2017-07-05 21:35:07 -0300

    update rofi

--- fish/config.fish
+++ fish/config.fish
@@ -14,13 +14,10 @@ end
 
 set -gx SSH_AUTH_SOCK /run/user/1000/keyring/ssh
 set -gx EDITOR vim
-set -gx ANT_ROOT /usr/bin
-set -gx NDK_ROOT /opt/android-ndk
-set -gx ANDROID_SDK_ROOT /opt/android-sdk
 set -gx ARON_GEM_PATH ~/.gem/ruby/2.4.0/bin
-set -gx CARGO_PATH /home/aron/.cargo/bin
-set -gx RUST_SRC_PATH /usr/local/src/rustc-1.7.0/src
 set -gx ARON_PYTHON_PATH /home/aron/.local/bin
+set -gx SPARK_HOME /home/aron/Tools/spark
+set -gx PYSPARK_SUBMIT_ARGS '--master local[8]'
 
 set -gx LUA_PATH '/home/aron/.luarocks/share/lua/5.1/?.lua;/home/aron/.luarocks/share/lua/5.1/?/init.lua;/home/aron/Programming/GitHub/torch_distro/install/share/lua/5.1/?.lua;/home/aron/Programming/GitHub/torch_distro/install/share/lua/5.1/?/init.lua;./?.lua;/home/aron/Programming/GitHub/torch_distro/install/share/luajit-2.1.0-beta1/?.lua;/usr/local/share/lua/5.1/?.lua;/usr/local/share/lua/5.1/?/init.lua'
 set -gx LUA_CPATH '/home/aron/.luarocks/lib/lua/5.1/?.so;/home/aron/Programming/GitHub/torch_distro/install/lib/lua/5.1/?.so;./?.so;/usr/local/lib/lua/5.1/?.so;/usr/local/lib/lua/5.1/loadall.so'
@@ -29,10 +26,11 @@ set -gx CUDA_BIN_PATH /opt/cuda/bin
 set -gx LD_LIBRARY_PATH /home/aron/Programming/GitHub/torch_distro/install/lib $LD_LIBRARY_PATH
 set -gx DYLD_LIBRARY_PATH /home/aron/Programming/GitHub/torch_distro/install/lib $DYLD_LIBRARY_PATH
 set -gx LUA_CPATH '/home/aron/Programming/GitHub/torch_distro/install/lib/?.so;' $LUA_CPATH
+set -gx CONDA_PATH /opt/anaconda/bin
 
 
 set -gx PATH $ARON_GEM_PATH $PATH*/
-# set -gx PATH $CARGO_PATH $PATH
+# set -gx PATH $CONDA_PATH $PATH
 set -gx PATH $ARON_PYTHON_PATH $PATH
 set -gx NODE_PATH /usr/lib/node_modules/
 
@@ -69,4 +67,8 @@ function gnome-keyring-daemon
   if test -n "$DESKTOP_SESSION"
     set -gx SSH_AUTH_SOCK (gnome-keyring-daemon --start | awk -F= '{print  $2}')
   end
+end
+
+function activate_pyspark
+    set -gx PATH $SPARK_HOME/bin $PATH
 end
