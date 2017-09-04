da14a1ee9056682952668496ecd88a6d641a9617
Aron Bordin <aron.bordin@gmail.com>
2017-09-04 12:52:53 -0300

    fix gem path

--- .vimrc
+++ .vimrc
@@ -263,6 +263,19 @@ nnoremap tj :tabprev<CR>
 nnoremap th :tabfirst<CR>
 nnoremap tl :tablast<CR>
 
+:nmap <ScrollWheelUp> <nop>
+:nmap <S-ScrollWheelUp> <nop>
+:nmap <C-ScrollWheelUp> <nop>
+:nmap <ScrollWheelDown> <nop>
+:nmap <S-ScrollWheelDown> <nop>
+:nmap <C-ScrollWheelDown> <nop>
+:nmap <ScrollWheelLeft> <nop>
+:nmap <S-ScrollWheelLeft> <nop>
+:nmap <C-ScrollWheelLeft> <nop>
+:nmap <ScrollWheelRight> <nop>
+:nmap <S-ScrollWheelRight> <nop>
+:nmap <C-ScrollWheelRight> <nop>
+
 nnoremap <leader>rv :source $MYVIMRC<CR>
 inoremap <C-o> <Esc>O<Esc>jA
 
--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,5 @@
 function su
-        /bin/su --shell=/usr/bin/fish $argv
-end
-
-function restart_inotify
-	sudo sysctl fs.inotify.max_user_watches=1000000
+        /bin/su --shell=/usr/bin/bash $argv
 end
 
 function update_pip
@@ -18,19 +14,12 @@ set -gx ARON_GEM_PATH ~/.gem/ruby/2.4.0/bin
 set -gx ARON_PYTHON_PATH /home/aron/.local/bin
 set -gx SPARK_HOME /home/aron/Tools/spark
 set -gx PYSPARK_SUBMIT_ARGS '--master local[8]'
-
-set -gx LUA_PATH '/home/aron/.luarocks/share/lua/5.1/?.lua;/home/aron/.luarocks/share/lua/5.1/?/init.lua;/home/aron/Programming/GitHub/torch_distro/install/share/lua/5.1/?.lua;/home/aron/Programming/GitHub/torch_distro/install/share/lua/5.1/?/init.lua;./?.lua;/home/aron/Programming/GitHub/torch_distro/install/share/luajit-2.1.0-beta1/?.lua;/usr/local/share/lua/5.1/?.lua;/usr/local/share/lua/5.1/?/init.lua'
-set -gx LUA_CPATH '/home/aron/.luarocks/lib/lua/5.1/?.so;/home/aron/Programming/GitHub/torch_distro/install/lib/lua/5.1/?.so;./?.so;/usr/local/lib/lua/5.1/?.so;/usr/local/lib/lua/5.1/loadall.so'
-set -gx PATH /home/aron/Programming/GitHub/torch_distro/install/bin $PATH
+ 
 set -gx CUDA_BIN_PATH /opt/cuda/bin
-set -gx LD_LIBRARY_PATH /home/aron/Programming/GitHub/torch_distro/install/lib $LD_LIBRARY_PATH
-set -gx DYLD_LIBRARY_PATH /home/aron/Programming/GitHub/torch_distro/install/lib $DYLD_LIBRARY_PATH
-set -gx LUA_CPATH '/home/aron/Programming/GitHub/torch_distro/install/lib/?.so;' $LUA_CPATH
-set -gx CONDA_PATH /opt/anaconda/bin
+set -gx CONDA_PATH /home/aron/Tools/miniconda3/bin
 
 
-set -gx PATH $ARON_GEM_PATH $PATH*/
-# set -gx PATH $CONDA_PATH $PATH
+set -gx PATH $ARON_GEM_PATH $PATH
 set -gx PATH $ARON_PYTHON_PATH $PATH
 set -gx NODE_PATH /usr/lib/node_modules/
 
@@ -54,15 +43,6 @@ function use_gcc5
     set -gx CXX g++-5
 end
 
-function start_agent                                                                                                                                                                    
-    echo "Initializing new SSH agent ..."
-    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-    echo "succeeded"
-    chmod 600 $SSH_ENV 
-    . $SSH_ENV > /dev/null
-    ssh-add
-end
-
 function gnome-keyring-daemon
   if test -n "$DESKTOP_SESSION"
     set -gx SSH_AUTH_SOCK (gnome-keyring-daemon --start | awk -F= '{print  $2}')
@@ -72,3 +52,6 @@ end
 function activate_pyspark
     set -gx PATH $SPARK_HOME/bin $PATH
 end
+
+abbr setclip "xclip -selection c"
+abbr getclip "xclip -selection c -o"
